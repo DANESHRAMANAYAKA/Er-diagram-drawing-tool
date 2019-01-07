ac59d06d8dc172e8b36cbfd5599e8358c3267cc2
DANESHRAMANAYAKA <drandinu28@gmail.com>
2019-01-08 02:15:36 +0530

    Er diagram drawing tool (java se)

---
+++ ErMaker/src/ermaker/TableProperties.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ermaker;
+
+/**
+ *
+ * @author root
+ */
+public class TableProperties {
+
+    private int locX = 0;
+    private int locY = 0;
+    private int width = 0;
+    private int height = 0;
+    private int row = 0;
+
+    public TableProperties() {
+    }
+
+    public int getLocX() {
+        return locX;
+    }
+
+    public void setLocX(int locX) {
+        this.locX = locX;
+    }
+
+    public int getLocY() {
+        return locY;
+    }
+
+    public void setLocY(int locY) {
+        this.locY = locY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+}
